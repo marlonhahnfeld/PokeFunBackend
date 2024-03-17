abf935c0089cbbb5bc817a97728ecbd6a54d06e1
KevinZawieja <kevin_zawieja@hotmail.de>
2024-03-17 15:54:55 +0100

    -relocated

--- PokeFunBackend/index.py
+++
@@ -1,1 +0,0 @@
-from PokeFunBackend.wsgi import app
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
-from PokeFunBackend.mongo import app
-from PokeFunBackend.mongo import CORS
+from mongo import app
+from mongo import CORS
 
 if __name__ == '__main__':
     CORS(app, supports_credentials=True)  
