cfe2bd7b509c7e19df293c21660708d82a9d3e0e
KevinZawieja <kevin_zawieja@hotmail.de>
2024-03-17 16:53:02 +0100

    - cors origins

--- mongo.py
+++ mongo.py
@@ -16,7 +16,7 @@ client = MongoClient(uri,server_api=ServerApi('1')) # Connect to the MongoDB clu
 db = client['Pokemon']
 collectionPokemon = db['Pokemon']
 collectionUsers = db['Users']
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True, origins=['https://poke-fun-blush.vercel.app'])
 
 
 SECRET_KEY = "oU7ufaHTqk7lE0OM7as5Kl1AY43G7UfO"
